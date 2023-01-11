203f4d7d4c9f8197a65c3b65add5ea0862a849f6
Gabriel Chazarreta <gfchazarreta09@gmail.com>
2023-01-10 23:35:03 -0300

    Updates

---
+++ static/js/200.67fccdaf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgreydive_challenge=self.webpackChunkgreydive_challenge||[]).push([[200],{2200:function(e,n,r){r.r(n),r.d(n,{default:function(){return c}});var a=r(1087),i=r(184);function c(){return(0,i.jsxs)("div",{className:"error404__container",children:[(0,i.jsx)("p",{children:"P\xe1gina no existente"}),(0,i.jsx)(a.rU,{className:"error404__btn",to:"/",children:"Volver a la p\xe1gina principal"})]})}}}]);
+//# sourceMappingURL=200.67fccdaf.chunk.js.map
--- static/js/799.4ec92d38.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgreydive_challenge=self.webpackChunkgreydive_challenge||[]).push([[799],{3313:function(e,n,i){i.r(n),i.d(n,{default:function(){return t}});var r=i(184);function t(){return(0,r.jsx)("div",{children:"P\xe1gina no existente"})}}}]);
-//# sourceMappingURL=799.4ec92d38.chunk.js.map
